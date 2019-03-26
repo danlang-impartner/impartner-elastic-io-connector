81aa8dec6fc9c29c21f2c5d300e1084daebf6c08
Dan Lang <dan.lang@impartner.com>
2019-03-26 16:57:02 -0600

    updated verify credentials and inputs

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,34 +1,48 @@
 "use strict";
 const request = require('request-promise');
+var util      = require('util');
 
 module.exports = verify;
 
 /**
- * Executes the verification logic by sending a simple to the Petstore API using the provided apiKey.
- * If the request succeeds, we can assume that the apiKey is valid. Otherwise it is not valid.
+ * Executes the verification logic by sending a simple request to Impartner PRM using the credentials provided.
+ * If the request succeeds, we can assume that the credentials are valid. Otherwise, they are not valid.
  *
- * @param credentials object to retrieve apiKey from
+ * @param credentials object to retrieve auth connection variables from
  *
  * @returns Promise sending HTTP request and resolving its response
  */
 function verify(credentials) {
 
-    // access the value of the apiKey field defined in credentials section of component.json
-    const apiKey = credentials.apiKey;
+    // get the value of our credentials variables defined in credentials section of component.json
+    const username        = credentials.username;
+    const password        = credentials.password;
+    const environmentType = credentials.environmentType;
+    const apiVersion      = credentials.apiVersion;
 
-    if (!apiKey) {
-        throw new Error('API key is missing');
+    //call to format auth header. Put this into a utility. Doesnt belong here long term
+    var credentials = util.format("%s:%s", username, password);
+    var headerAuth  = 'Basic ' + new Buffer(credentials).toString('base64');
+
+    if (!username) {
+        throw new Error('Username is missing');
+    }else if(!password){
+        throw new Error('Password is missing');
+    }else if(!environmentType){
+        throw new Error('Environment Type is missing');
+    }else if(!apiVersion){
+        throw new Error('API Version is missing');
     }
 
     // sending a request to the most simple endpoint of the target API
     const requestOptions = {
-        uri: 'https://petstore.elastic.io/v2/user/me',
+        uri: environmentType + '/objects/' + apiVersion +'/PartnerLevel',
         headers: {
-            'api-key': apiKey
+            'Authorization': headerAuth
         },
         json: true
     };
 
-    // if the request succeeds, we can assume the api key is valid
+    // if the request succeeds, we can assume the credentials are valid
     return request.get(requestOptions);
 }
